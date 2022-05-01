b0a3ed8b6bb4696c94968fc9eb5a2fc627e74530
ilunatrius <ilunatrius@gmail.com>
2022-05-01 21:58:31 +0700

    d

--- installer.sh
+++ installer.sh
@@ -24,6 +24,20 @@ function installPackages()
   sudo pacman -S $1 $2 $3 $4 $5 $6 $7 $8 $9 --noconfirm
 }
 
+function installAURPackage()
+{
+  if [[ -z $1 ]]; then
+    echo "no arguments"
+    return
+  fi
+
+  sudo git clone $1 build
+  cd build
+  sudo makepkg -si --noconfirm
+  cd ../
+  sudo rm -rf build
+}
+
 function launchService()
 {
   if [[ -z $1 ]]; then
@@ -31,23 +45,42 @@ function launchService()
     return
   fi
 
-  sudo systemctl enable $1 --noconfirm
-  sudo systemctl start $1 --noconfirm
+  sudo systemctl enable $1
+  sudo systemctl start $1
 }
 
 function hideGrubLoader()
 {
-  grubPath = /etc/default/grub
-  
-  sudo cp grubPath ~/temp1
+  sudo cp /etc/default/grub ~/temp1
   
   sudo echo "GRUB_DEFAULT=0" >> ~/temp1
   sudo echo "GRUB_TIMEOUT=0" >> ~/temp1
   sudo echo "GRUB_DISABLE_OS_PROBER=true" >> ~/temp1
 
-  sudo mv ~/temp1 grubPath
+  sudo mv ~/temp1 /etc/default/grub
 
-  sudo grub-mkconfig -o /boot/grub/grub.cfg --noconfirm
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+function copyPolybarConfig()
+{
+  sudo mkdir ~/.configs/polybar
+  sudo cp -rf ./data/grayblocks ~/.configs/polybar
+}
+
+function copyI3Configs()
+{
+  sudo mkdir ~/Configs
+  sudo cp -rf ./data/Configs ~/
+  sudo cp .data/i3-config ~/.configs/i3/config
+}
+
+function setWallpaper()
+{
+  sudo mkdir ~/Pictures
+  sudo mkdir ~/Pictures/Wallpapers
+  sudo cp ./data/wallpaper.jpg ~/Pictures/Wallpapers/wallpaper.jpg
+  sudo nitrogen --set-zoom-fill ~/Pictures/Wallpapers/wallpaper.jpg
 }
 
 # check sudo
@@ -67,11 +100,28 @@ update
 
 # install packages
 installPackages xorg-server xorg-xinit xclip
-installPackages wget python base-devel git
+installPackages wget python base-devel git maim 
 installPackages kitty rofi neofetch nitrogen feh
 installPackages neovim lightdm lightdm-gtk-greeter
-installPackages i3-gaps i3blocks i3lock gtk4
+installPackages i3-gaps i3blocks i3lock gtk4 galculator
+installPackages ttf-hanazono ttf-sazanami ttf-font-awesome ttf-fira-code ttf-anonymous-pro
+installPackages telegram-desktop nautilus
 
 # setup display and window manager
 sudo echo i3 >> ~/.xinitrc
 launchService lightdm
+
+# install AUR packages
+installAURPackage https://aur.archlinux.org/waterfox-g4-bin.git
+installAURPackage https://aur.archlinux.org/visual-studio-code-bin.git
+installAURPackage https://aur.archlinux.org/ttf-ms-fonts.git
+installAURPackage https://aur.archlinux.org/ttf-iosevka.git
+installAURPackage https://aur.archlinux.org/ttf-icomoon-feather.git
+installAURPackage https://aur.archlinux.org/polybar.git
+
+# settings
+copyPolybarConfig
+copyI3Configs
+setWallpaper
+
+# sudo reboot
